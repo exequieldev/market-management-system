034c094844fb7d87a1bf81e618840579f2fd412a
Exequiel Diaz <exequielandresdiaz@gmail.com>
2024-01-19 21:07:34 -0300

    Se agrego auntentificacion y plantilla de usuario

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('auth.login');
+    return Redirect('login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
